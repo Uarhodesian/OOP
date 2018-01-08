eb61f8bf94399af506534f47419622240a63743d
Oleksii Danylyuk <danilyuk@bigmir.net>
2018-01-08 21:51:11 +0200

    almost smoothly

--- OOP1.py
+++ OOP1.py
@@ -16,6 +16,7 @@
  j) Redefine string representation for employee as follows:
 "@firstName@ @secondName@, manager:@manager secondName@, experiance:@experiance@"
  """
+
 class employee:
     number_of_employee = 0
     def __init__(self, fname, lname, salary, experiance, manager):
@@ -35,44 +36,48 @@ class employee:
             self.salary = self.salary  + 200
             return self.salary
         else:
-            return self.salary
-        
+            return self.salary       
     def __repr__(self):
-        return "employee('{}','{}','{}')".format(self.fname, self.lname, self.experiance)          
+        return "employee('{}','{}','{}')".format(self.fname, self.lname, self.experiance)        
     def __str__(self):
         return "Hi! I am an employee"
 
 class developer(employee):
+    number_of_developer = 0
+    def __init__(self, fname, lname, salary, experiance, manager):
+        employee.__init__(self, fname, lname, salary, experiance, manager)
+        developer.number_of_developer += 1
     def __str__(self):
-        return "Hi! I am a developer"
+        return "developer"
     
 class designer(employee):
+    number_of_designer = 0
     def __init__(self, fname, lname, salary, experiance, manager, effect_coefficient):
         #supper().__init__(self, fname, lname, salary, experiance, manager)
         employee.__init__(self, fname, lname, salary, experiance, manager)
         self.effect_coefficient = effect_coefficient
+        designer.number_of_designer += 1
     def full_salary(self):
         self.salary = self.salary * self.effect_coefficient
         return self.salary
     def __str__(self):
-        return "Hi! I am a designer"
+        return "designer"
     
 class manager(employee):
     count_dev = 0
     count_des = 0
+    count_emp = 0
     def __init__(self, fname, lname, salary, experiance, manager, employees=None):
-        employee.__init__(self, fname, lname, salary, experiance, manager)
-
+        employee.__init__(self, fname, lname, salary, experiance, manager)     
         if employees is None:
             self.employees = []          
         else:
             self.employees = employees
-          
-        if  isinstance(self.employees, designer):
-            self.employees.count_des += 1                  
-        elif  isinstance(self.employees, developer):
-            self.employees.count_dev += 1    
-    #count_emp = count_des + count_dev
+  
+    def count_emp(self):
+            for employees in self.employees:
+                count_emp += 1
+                print(count_emp)
 
     def add_emp(self, emp):
             if emp not in self.employees:
@@ -86,56 +91,30 @@ class manager(employee):
             for employees in self.employees:
                 print('--->', employees)
 
-
-    def full_salary(self):     
-        if self.experiance > 5:
-            self.salary = self.salary * 1.2 + 500
-            return self.salary
-        elif self.experiance > 2:
-            self.salary = self.salary  + 200
-            return self.salary
-        else:
-            return self.salary
-
+    def count_emp(self):
+            for employees in self.employees:
+                if isinstance(employees, designer):
+                    self.count_des += 1
+                elif  isinstance(employees, developer):
+                    self.count_dev += 1
              
-
-"""class department(manager):
-    def __init__(self, fname, lname, manager=None):
-        employee.__init__(self, fname, lname)
-        if manager is None:
-            self.manager = []
-            number_of_employees = 0
-        else:
-            self.manager = manager
-            self.number_of_employees += 1
-            print(self.number_of_employees)
-        def add_man(self, man):
-            if man not in self.manager:
-                self.manager.append(man)
-
-        def remove_man(self, man):
-            if emp in self.manager:
-                self.manager.remove(man)
-
-        def print_man(man):
-            for man in self.manager:
-                print('--->'.man.fullname())
-
-"""                
+                 
 vova = developer('Vova', 'Pu', 300, 7, 'Budkin')                
 anton = designer('Anton', 'Antonovich', 350, 2, 'Budkin', 1)   
 vasya = developer('Vasya', 'Pupkin', 300, 5, 'Budkin')
 petya = developer('Petro', 'Petrovich', 200, 3, 'Budkin')
 maria = designer('Maria', 'Antonovich', 200, 1, 'Budkin', 0.7)        
 Budkin = manager('Vasya', 'Budkin', 300, 10, 'Boss', ['vova', 'anton', 'petya', 'maria'])
-zhuk = manager('Mykola', 'Zhuk', 500, 12, 'Boss')     
+zhuk = manager('Mykola', 'Zhuk', 500, 12, 'Boss', ['Budkin'])     
 
 print(employee.number_of_employee)
+print(designer.number_of_designer)
+print(developer.number_of_developer)
 print(anton.__repr__())
 print(anton.full_salary())
 print(Budkin.__repr__())
 print(Budkin.full_salary())
-print(Budkin.count_dev)
-print(Budkin.count_des)
+print(Budkin.count_emp)
 print(Budkin.print_emp())
+
 #print(Budkin.count_emp)
